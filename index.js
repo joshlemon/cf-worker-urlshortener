594d1404550822cf5630fbd09f503638a52c9b70
Josh Lemon <joshlemon@users.noreply.github.com>
2024-02-06 11:27:04 +1100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function removeTrailingSlash(str) {
 
 function defaultReturn(){
   //return new Response("Not Found", {status: 404})
-  return Response.redirect("https://www.joshlemon.com.au", 302);
+  return Response.redirect("https://joshlemon.com.au", 301);
 }
 
 addEventListener('fetch', event => {
@@ -15,37 +15,35 @@ async function handleRedirect(request) {
   
   if (pathname !== ""){ //check that the path isn't blank
     let location = await urlstore.get(pathname.toLowerCase()); //get the path and convert to lowercase
-    console.log(`Location equals: ${location}`);
-
+    
     if (location == null){ //if there is no entry for the path, return to default URL location
-      await tracker("UNKNOWN");
+      //await tracker("UNKNOWN"); //removed to reduce KV usage
       return defaultReturn();
     }
     
+    console.log(`Sending to Tracker`);
     await tracker(pathname.toLowerCase());
     return Response.redirect(location, 302); //if path exists in store, redurect the request per the store
   
   } else { //if path is blank return to default URL location
-    await tracker("BLANK");
+    //await tracker("BLANK"); //removed to reduce KV usage
     return defaultReturn();
   }
 }
 
 async function tracker(urltrack){
+  console.log(`Getting ${urltrack} from Tracker`);
   let urlexists = await urlcounter.get(urltrack);
-  console.log(`Does the URL exist: ${urlexists}`);
-     
-  if (urlexists == null){
-    console.log(`Adding ${urltrack} to KV.`);
-    urlcounter.put(urltrack, "1");
-    return;
+  console.log(`${urltrack} exists: ${urlexists}`);
 
-  } else {
-    console.log(`Incramenting ${urltrack} to KV.`);
+  if (urlexists != null){
     let val1 = parseInt(urlexists.toString())
     val1 = val1 + 1
     await urlcounter.put(urltrack, val1.toString());
-    console.log(`New value is ${val1.toString()}`)
     return;
+
+  } else {
+    await urlcounter.put(urltrack, "1");
+    return;    
   }
 }
